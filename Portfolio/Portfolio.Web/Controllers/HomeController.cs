4a7d09a89fd5a562cbfa527a66e0507bf37857e5
Keshav Sharma <sharma.keshav39@gmail.com>
2023-07-01 23:34:46 +0530

    Add Partial View and View Component (#5)
    Co-authored-by: Keshav Sharma <sharma.keshav39@live.com>

--- Portfolio/Portfolio.Web/Controllers/HomeController.cs
+++ Portfolio/Portfolio.Web/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace Portfolio.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult ViewTypes()
+        {
+            return View();
+        }
     }
 }
---
+++ Portfolio/Portfolio.Web/Controllers/SocialLinksViewComponent.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Web.Controllers;
+
+public class SocialLinksViewComponent : ViewComponent
+{
+    List<SocialIcon> socialIcons = new List<SocialIcon>();
+    public SocialLinksViewComponent()
+    {
+        socialIcons = SocialIcon.AppSocialIcons();
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var model = socialIcons;
+        return await Task.FromResult((IViewComponentResult)View("SocialLinks", model));
+    }
+
+}
