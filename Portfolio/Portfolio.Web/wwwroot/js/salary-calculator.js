e91911325ccbc8181f69786beaabc86246dcb820
Keshav Sharma <sharma.keshav39@gmail.com>
2023-08-25 23:43:41 +0530

    Cleanup CostToCompany
    Use Bootstrap to design UI
    Use Javascript instead of jQuery
    Move javascript code to new file

--- Portfolio/Portfolio.Web/wwwroot/js/salary-calculator.js
+++ Portfolio/Portfolio.Web/wwwroot/js/salary-calculator.js
@@ -1,30 +1,68 @@
-$("#salaryRange").on('change', salaryRangeChanged);
-$("#salaryInput").on('change', salaryInputChanged);
+var salarySlider = document.getElementById('salarySlider');
+var salaryInput = document.getElementById('salaryInput');
 
-function salaryRangeChanged() {
-    var newSalary = $("#salaryRange").val();
-    $("#salaryInput").val(newSalary);
+salarySlider.addEventListener('input', salarySliderChanged);
+salaryInput.addEventListener('input', salaryInputChanged);
+
+function salarySliderChanged() {
+    salaryInput.value = salarySlider.value;
     bifurcateSalary();
 }
 
 function salaryInputChanged() {
-    var newSalary = $("#salaryInput").val();
-    $("#salaryRange").val(newSalary);
+    salarySlider.value = salaryInput.value;
     bifurcateSalary();
 }
-//TODO: Convert this code to Javascript
 
 bifurcateSalary();
 function bifurcateSalary() {
-    //TODO: Put this script in a different file
-    var grossSalary = Number($("#salaryRange").val());
-    var basicSalary = Math.round(grossSalary / 2);
-    var oneSidedPF = Math.round(basicSalary * 0.12);
-    var totalSalary = grossSalary + oneSidedPF;
-    //TODO: Show P.A (Per Annum) and P.M (Per Month) salary details
-    $("#baseSalary").text(basicSalary.toLocaleString('en-IN'));
-    $("#grossSalary").text(grossSalary.toLocaleString('en-IN'));
-    $("#employeePF").text(oneSidedPF.toLocaleString('en-IN'));
-    $("#employerPF").text(oneSidedPF.toLocaleString('en-IN'));
-    $("#totalSalary").text(totalSalary.toLocaleString('en-IN'));
+
+    var grossYearly = Number(salarySlider.value);
+    var grossMonthly = Math.round(grossYearly / 12);
+
+    document.getElementById('grossYearly').textContent = grossYearly.toLocaleString('en-IN');
+    document.getElementById('grossMonthly').textContent = grossMonthly.toLocaleString('en-IN');
+
+    var basicYearly = Math.round(grossYearly / 2);
+    var basicMonthly = Math.round(basicYearly / 12);
+    document.getElementById('basicYearly').textContent = basicYearly.toLocaleString('en-IN');
+    document.getElementById('basicMonthly').textContent = basicMonthly.toLocaleString('en-IN');
+
+    var pfYearly = Math.round(basicYearly * 0.12);
+    var pfMonthly = pfYearly / 12;
+    document.getElementById('pfYearly').textContent = pfYearly.toLocaleString('en-IN');
+    document.getElementById('pfMonthly').textContent = pfMonthly.toLocaleString('en-IN');
+
+    var tdsYearly = calculateTDS(grossYearly);
+    var tdsMonthly = Math.round(tdsYearly / 12);
+    document.getElementById('tdsYearly').textContent = tdsYearly.toLocaleString('en-IN');
+    document.getElementById('tdsMonthly').textContent = tdsMonthly.toLocaleString('en-IN');
+
+    var incomeYearly = grossYearly - pfYearly - tdsYearly;
+    var incomeMonthly = grossMonthly - pfMonthly - tdsMonthly;
+    document.getElementById('incomeYearly').textContent = incomeYearly.toLocaleString('en-IN');
+    document.getElementById('incomeMonthly').textContent = incomeMonthly.toLocaleString('en-IN');
+}
+
+function calculateTDS(salary) {
+    var applicableTDS = 0;
+    var fifteenLakhs = 1500000;
+    var twelveLakhs = 1200000;
+    var nineLakhs = 900000;
+    var sixLakhs = 600000;
+    var threeLakhs = 300000;
+
+    if (salary > fifteenLakhs) {
+        applicableTDS = 150500 + (salary - fifteenLakhs) * 0.30;
+    } else if (salary > twelveLakhs) {
+        applicableTDS = 90000 + (salary - twelveLakhs) * 0.20;
+    } else if (salary > nineLakhs) {
+        applicableTDS = 45000 + (salary - nineLakhs) * 0.15;
+    } else if (salary > sixLakhs) {
+        applicableTDS = 15000 + (salary - sixLakhs) * 0.10;
+    } else if (salary > threeLakhs) {
+        applicableTDS = (salary - threeLakhs) * 0.05;
+    }
+
+    return applicableTDS;
 }
