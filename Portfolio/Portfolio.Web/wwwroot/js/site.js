c791c36087373ba84b59cf188fe92e4570705d36
Keshav Sharma <sharma.keshav39@gmail.com>
2023-09-10 16:24:18 +0530

    Add self introduction with a typing effect.

--- Portfolio/Portfolio.Web/wwwroot/js/site.js
+++ Portfolio/Portfolio.Web/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function tabLoadEventHandler() {
+    let hash = 'tab_' + +new Date();
+    sessionStorage.setItem('TabHash', hash);
+    let tabs = JSON.parse(localStorage.getItem('TabsOpen') || '{ }');
+    tabs[hash] = true;
+    localStorage.setItem('TabsOpen', JSON.stringify(tabs));
+}
+function tabUnloadEventHandler() {
+    let hash = sessionStorage.getItem('TabHash');
+    let tabs = JSON.parse(localStorage.getItem('TabsOpen') || '{ }');
+    delete tabs[hash];
+    localStorage.setItem('TabsOpen', JSON.stringify(tabs));
+
+    let tabsCount = Object.keys(JSON.parse(localStorage.getItem('TabsOpen') || '{}')).length
+}
