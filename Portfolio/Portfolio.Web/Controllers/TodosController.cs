3baa55da5b3a2f156d939a2574c8343e94487a03
Keshav Sharma <sharma.keshav39@gmail.com>
2024-08-09 22:35:39 +0530

    Show Dummy Todos on a page

---
+++ Portfolio/Portfolio.Web/Controllers/TodosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Web.Services;
+
+namespace Portfolio.Web.Controllers;
+
+public class TodosController(ITodosService todosService): Controller
+{
+    private readonly ITodosService _todosService = todosService;
+
+    [HttpGet]
+    public async Task<IActionResult> ShowTodos()
+    {
+        var todos = await _todosService.GetTodos();
+        return View(todos);
+    }
+}
