3d21f3f473564e993e41cbf7ed0355f7166cf969
Keshav Sharma <sharma.keshav39@gmail.com>
2025-01-17 23:14:05 +0530

    Trim Playwright config: Removed most properties (#82)

--- playwright-setup/tests/UIBasicsTest.spec.js
+++ playwright-setup/tests/UIBasicsTest.spec.js
@@ -1,11 +1,15 @@
-const { test } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
 test('Browser Context Playwright Test', async ({browser}) => {
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto("https://www.google.com/");
+    await page.goto("https://www.google.com/maps");
+
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google Maps");
 });
 
-test('Page Playwright Test', async ({page}) => {
+test.only('Page Playwright Test', async ({page}) => {
     await page.goto("https://www.google.com/");
+    console.log(await page.title());
 });
