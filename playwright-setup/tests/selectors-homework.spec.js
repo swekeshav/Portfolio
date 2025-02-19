a58f60475629cd086f6d65586ebee3db7f13d6ec
Keshav Sharma <sharma.keshav39@gmail.com>
2025-02-19 21:14:01 +0530

    Add Item to Cart

--- playwright-setup/tests/selectors-homework.spec.js
+++ playwright-setup/tests/selectors-homework.spec.js
@@ -14,3 +14,26 @@ test('Homework, login to website and read product Title', async ({ page }) => {
     await cardTitles.last().waitFor();
     console.log(await cardTitles.allTextContents());
 });
+
+test.only('Homework, complete a user journey', async ({ page }) => {
+    const userId = 'neil@nitin.com';
+    const pwd = 'Neil@123';
+
+    await page.goto("https://rahulshettyacademy.com/client"); 
+
+    await page.locator('#userEmail').fill(userId);
+    await page.locator('#userPassword').fill(pwd);
+    await page.locator('#login').click();
+
+    const cardTitles = page.locator('.card .card-body h5');
+    await expect(cardTitles.last()).toBeVisible();
+
+    const chosenItem = page.getByText('ZARA COAT 3');
+    await page.locator('div.card-body')
+        .filter({ has: chosenItem })
+        .locator('button:has-text("Add To Cart")')
+        .click();
+    
+    await page.locator('button:text-is("Cart")').click();
+    await page.pause();
+});
