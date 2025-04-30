43e4c2e483dd42262500b76c76b28913aa7a9896
Keshav Sharma <sharma.keshav39@gmail.com>
2025-04-30 21:30:37 +0530

    Remove unnecessary usings

--- Portfolio/Portfolio.WPF/Calculator.xaml.cs
+++ Portfolio/Portfolio.WPF/Calculator.xaml.cs
@@ -1,27 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Portfolio.WPF
 {
-    /// <summary>
-    /// Interaction logic for Calculator.xaml
-    /// </summary>
-    public partial class Calculator : Window
-    {
-        public Calculator()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for Calculator.xaml
+	/// </summary>
+	public partial class Calculator : Window
+	{
+		public Calculator()
+		{
+			InitializeComponent();
+		}
+	}
 }
