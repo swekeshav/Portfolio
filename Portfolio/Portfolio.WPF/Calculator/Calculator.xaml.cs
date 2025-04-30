c3a707e597ea7ce8fc3e7be53de6f8e38accdf9d
Keshav Sharma <sharma.keshav39@gmail.com>
2025-04-30 22:41:29 +0530

    Display numbers on click

--- Portfolio/Portfolio.WPF/Calculator/Calculator.xaml.cs
+++ Portfolio/Portfolio.WPF/Calculator/Calculator.xaml.cs
@@ -1,15 +1,34 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Portfolio.WPF
 {
-	/// <summary>
-	/// Interaction logic for Calculator.xaml
-	/// </summary>
-	public partial class Calculator : Window
-	{
-		public Calculator()
-		{
-			InitializeComponent();
-		}
-	}
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : Window
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        void BtnNumber_Click(object sender, RoutedEventArgs args)
+        {
+            var newContent = (sender as Button)?.Content;
+            var currentContent = lblResult.Content;
+
+            lblResult.Content = currentContent.Equals("0")
+                ? newContent
+                : $"{currentContent}{newContent}";
+        }
+
+        void BtnPoint_Click(object sender, RoutedEventArgs args)
+        {
+            var currentContent = lblResult.Content.ToString()!;
+            if (currentContent.Contains('.'))
+                return;
+            lblResult.Content = $"{currentContent}.";
+        }
+    }
 }
