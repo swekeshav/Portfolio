2ea531df406267b8a1840cbd6b48ba0ac5f5d64b
Keshav Sharma <sharma.keshav39@gmail.com>
2025-06-05 12:24:42 +0530

    Add dueDate & reviewDate for comparison in future

--- Portfolio/Portfolio.Web/Controllers/TodosController.cs
+++ Portfolio/Portfolio.Web/Controllers/TodosController.cs
@@ -4,21 +4,21 @@ using Portfolio.Web.Services;
 
 namespace Portfolio.Web.Controllers;
 
-public class TodosController(ITodosService todosService): Controller
+public class TodosController(ITodosService todosService) : Controller
 {
-    private readonly ITodosService _todosService = todosService;
+	readonly ITodosService _todosService = todosService;
 
-    [HttpGet]
-    public async Task<IActionResult> ShowTodos()
-    {
-        var todos = await _todosService.GetTodos();
-        return View(todos);
-    }
+	[HttpGet]
+	public async Task<IActionResult> ShowTodos()
+	{
+		var todos = await _todosService.GetTodos();
+		return View(todos);
+	}
 
-    [HttpPost]
-    public async Task<IActionResult> AddTodo([FromForm]TodoInputViewModel newTodo)
-    {
-        await _todosService.AddTodo(newTodo);
-        return RedirectToAction("ShowTodos");
-    }
+	[HttpPost]
+	public async Task<IActionResult> AddTodo([FromForm] TodoInputViewModel newTodo)
+	{
+		await _todosService.AddTodo(newTodo);
+		return RedirectToAction("ShowTodos");
+	}
 }
--- Portfolio/Portfolio.Web/Models/TodoViewModel.cs
+++ Portfolio/Portfolio.Web/Models/TodoViewModel.cs
@@ -1,9 +1,13 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Portfolio.Web.Models;
 
 public class TodoViewModel
 {
-    public string Title { get; set; } = null!;
-    public Guid UUID { get; set; }
-    public bool IsCompleted { get; set; }
+	public string Title { get; set; } = null!;
+	public Guid UUID { get; set; }
+	public bool IsCompleted { get; set; }
+	public DateTime? DueDate { get; set; }
+	public DateTime? ReviewDate { get; set; }
 }
